5dbea56bb51768679579944d449d7eddf996443f
mahosushi <z2317x3@gmail.com>
2022-02-10 21:34:32 +0800

    0210 boo

--- cmds/task.py
+++ cmds/task.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from core.classes import Cog_Extension
 import json
 import asyncio
-import datetime
+from datetime import date, datetime
 
 
 # 初始化又繼承
@@ -11,17 +11,35 @@ import datetime
 class Task(Cog_Extension):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.counter = 0
+        # async def interval():
+        #     await self.bot.wait_until_ready()
+        #     # channel 打成字串就不行
+        #     self.channel = self.bot.get_channel(896373179818332172)
+        #     while not self.bot.is_closed():
+        #         await self.channel.send('嗨嗨，運行中')
+        #         await asyncio.sleep(5)
 
-        async def interval():
+        # # bot 去 loop 去創建 task
+        # self.bg_task = self.bot.loop.create_task(interval())
+
+        async def time_task():
             await self.bot.wait_until_ready()
-            # channel 打成字串就不行
             self.channel = self.bot.get_channel(896373179818332172)
             while not self.bot.is_closed():
-                await self.channel.send('嗨嗨，運行中')
-                await asyncio.sleep(5)
+                now_time = datetime.now().strftime("%H:%M")
+                with open('setting.json', 'r', encoding='utf8') as f:
+                    jdata = json.load(f)
 
-        # bot 去 loop 去創建 task
-        self.bg_task = self.bot.loop.create_task(interval())
+                if now_time == jdata['time'] and self.counter == 0:
+                    self.counter = 1
+                    await self.channel.send('Task Working!')
+                    await asyncio.sleep(1)
+                else:
+                    pass
+                    await asyncio.sleep(1)
+
+        self.bg_task = self.bot.loop.create_task(time_task())
 
     @commands.command()
     async def set_channel(self, ctx, ch: int):
@@ -29,6 +47,17 @@ class Task(Cog_Extension):
         self.channel = self.bot.get_channel(ch)
         # 去 tag 他
         await ctx.send(f'設置頻道 {self.channel.mention} 成功')
+
+    @commands.command()
+    async def set_time(self, ctx, time):
+        self.counter = 0
+        with open('setting.json', 'r', encoding='utf8') as f:
+            jdata = json.load(f)
+        jdata['time'] = time
+        await ctx.send(f'Set task time: {time}.')
+        with open('setting.json', 'w', encoding='utf8') as f:
+            # ident 設定多少
+            jdata = json.dump(jdata, f, indent=4)
 
 
 def setup(bot):
